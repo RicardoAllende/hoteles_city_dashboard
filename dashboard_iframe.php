f091cf0831eaaed2a682e5691ec8672c5ff38542
Ricardo Allende <ricardo.allende@subitus.com>
2019-11-12 10:59:16 -0600

    Ajuste max-width: 100%

--- dashboard_iframe.php
+++ dashboard_iframe.php
@@ -74,7 +74,7 @@ $PAGE->set_title(get_string('pluginname', 'local_dominosdashboard'));
     
     <?php echo local_hoteles_city_dashboard_print_theme_variables(); ?>
 </head>
-<body style="background-color: #ecedf1;" style="max-width: 100%; max-height: 100%;">
+<body style="background-color: #ecedf1; max-width: 100%; max-height: 100%;">
 
 <!-- onload="loaderGeneral()" -->
 
@@ -88,7 +88,7 @@ $PAGE->set_title(get_string('pluginname', 'local_dominosdashboard'));
      
 
     <!-- Inicia row para cards informativas -->
-    <div class="row" id="cards informativas" style="max-width: 100%;">
+    <div class="row container" id="cards informativas" style="max-width: 100%;">
 
         <!-- Card Número de hoteles-->
         <div class="col-sm-3 mb-4">
@@ -172,7 +172,7 @@ $PAGE->set_title(get_string('pluginname', 'local_dominosdashboard'));
     
     
     <!-- Div para pintar las graficas en dashboard -->
-    <div id="contenedor_graficas" class="row" style="padding: 15px 25px;"></div>
+    <div id="contenedor_graficas" class="row" style="padding: 15px 25px; max-width: 100%;"></div>
     
     
      
